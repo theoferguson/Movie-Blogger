1e9593da4d317300bb7f92d3227b9daf82ebac47
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-13 13:18:37 -0400

    added movies to db.json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import MovieContainer from './MovieContainer'
 
 function App() {
+//movies to be defined from a useEffect from local database to be populated from movie info from public API
 
-  
   return (
     <div className="App">
       <MovieContainer movies={movies} />
