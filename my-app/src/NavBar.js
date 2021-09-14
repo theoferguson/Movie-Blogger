789a8220e76dcd7adc5fda1dea5b928f8763f8b4
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-13 21:11:20 -0400

    added bootstrap

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -2,11 +2,11 @@ import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <nav>
-            <NavLink exact to="/">Home</NavLink>
-            <NavLink to="/reviews">Reviews</NavLink>
-            <NavLink to ="/my-movies">My Movies</NavLink>
-            <NavLink to ="/reviews/new">New Review</NavLink>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink className="navbar-brand" exact to="/">Home</NavLink>
+            <NavLink className="nav-link" to="/reviews">Reviews</NavLink>
+            <NavLink className="nav-link" to ="/my-movies">My Movies</NavLink>
+            <NavLink className="nav-link" to ="/reviews/new">New Review</NavLink>
         </nav>
     )
 };
